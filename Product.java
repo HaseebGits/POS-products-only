bd2a9cf7ac2637f5bd80cf883ebb60bc000d6673
HaseebGits <muhammad.haseeb@uworx.co.uk>
2025-08-27 15:29:34 +0500

    Here is a final code for review

---
+++ Product.java
@@ -0,0 +1,28 @@
+class Product{
+    private String id;
+    private String name;
+    private double price;
+    public Product(String id, String name, double price ){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
